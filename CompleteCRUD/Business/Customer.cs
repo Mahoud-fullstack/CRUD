8cecfdbac4f4b2dcbba010180524b229c9bc4892
Mahoud-fullstack <acc.msaaad@gmail.com>
2021-11-25 12:01:24 -0500

    pushing dot net proj to github using command line

---
+++ CompleteCRUD/Business/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteCRUD.Business
+{
+    public class Customer
+    {
+
+
+        public int CustometId     { get; set; }
+        public string FirstName   { get; set; }
+        public string LastName    { get; set; }
+        public string PhoneNmber  { get; set; }
+
+
+
+
+    }
+}
---
+++ CompleteCRUD/GUI/CustomerForm.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace CompleteCRUD.GUI
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCustomerId = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxPhoneNumber = new System.Windows.Forms.MaskedTextBox();
+            this.comboBoxChoice = new System.Windows.Forms.ComboBox();
+            this.buttonAddToList = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonListCustomers = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.listViewCustomer = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(242, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(242, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone Number";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelInfo.Location = new System.Drawing.Point(40, 330);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(86, 21);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "Input Info";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(40, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Search By";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(34, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Last Name";
+            // 
+            // textBoxCustomerId
+            // 
+            this.textBoxCustomerId.Location = new System.Drawing.Point(38, 74);
+            this.textBoxCustomerId.Name = "textBoxCustomerId";
+            this.textBoxCustomerId.Size = new System.Drawing.Size(148, 23);
+            this.textBoxCustomerId.TabIndex = 6;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(242, 74);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(148, 23);
+            this.textBoxFirstName.TabIndex = 7;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(40, 178);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(148, 23);
+            this.textBoxLastName.TabIndex = 8;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(40, 364);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(148, 23);
+            this.textBoxInput.TabIndex = 9;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // maskedTextBoxPhoneNumber
+            // 
+            this.maskedTextBoxPhoneNumber.Location = new System.Drawing.Point(242, 178);
+            this.maskedTextBoxPhoneNumber.Mask = "(999) 000-0000";
+            this.maskedTextBoxPhoneNumber.Name = "maskedTextBoxPhoneNumber";
+            this.maskedTextBoxPhoneNumber.Size = new System.Drawing.Size(148, 23);
+            this.maskedTextBoxPhoneNumber.TabIndex = 10;
+            // 
+            // comboBoxChoice
+            // 
+            this.comboBoxChoice.FormattingEnabled = true;
+            this.comboBoxChoice.Items.AddRange(new object[] {
+            "Customer ID",
+            "First Name",
+            "Last Name"});
+            this.comboBoxChoice.Location = new System.Drawing.Point(40, 266);
+            this.comboBoxChoice.Name = "comboBoxChoice";
+            this.comboBoxChoice.Size = new System.Drawing.Size(148, 23);
+            this.comboBoxChoice.TabIndex = 11;
+            this.comboBoxChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxChoice_SelectedIndexChanged);
+            // 
+            // buttonAddToList
+            // 
+            this.buttonAddToList.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonAddToList.Location = new System.Drawing.Point(501, 62);
+            this.buttonAddToList.Name = "buttonAddToList";
+            this.buttonAddToList.Size = new System.Drawing.Size(122, 43);
+            this.buttonAddToList.TabIndex = 12;
+            this.buttonAddToList.Text = "Add to List";
+            this.buttonAddToList.UseVisualStyleBackColor = true;
+            this.buttonAddToList.Click += new System.EventHandler(this.buttonAddToList_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSave.Location = new System.Drawing.Point(653, 62);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(122, 43);
+            this.buttonSave.TabIndex = 13;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonUpdate.Location = new System.Drawing.Point(653, 134);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(122, 43);
+            this.buttonUpdate.TabIndex = 15;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonListCustomers
+            // 
+            this.buttonListCustomers.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonListCustomers.Location = new System.Drawing.Point(501, 134);
+            this.buttonListCustomers.Name = "buttonListCustomers";
+            this.buttonListCustomers.Size = new System.Drawing.Size(122, 43);
+            this.buttonListCustomers.TabIndex = 14;
+            this.buttonListCustomers.Text = "List Customers";
+            this.buttonListCustomers.UseVisualStyleBackColor = true;
+            this.buttonListCustomers.Click += new System.EventHandler(this.buttonListCustomers_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSearch.Location = new System.Drawing.Point(242, 254);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(148, 43);
+            this.buttonSearch.TabIndex = 17;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonDelete.Location = new System.Drawing.Point(501, 195);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(122, 43);
+            this.buttonDelete.TabIndex = 16;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonExit.Location = new System.Drawing.Point(653, 195);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(122, 43);
+            this.buttonExit.TabIndex = 18;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // listViewCustomer
+            // 
+            this.listViewCustomer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewCustomer.HideSelection = false;
+            this.listViewCustomer.Location = new System.Drawing.Point(34, 416);
+            this.listViewCustomer.Name = "listViewCustomer";
+            this.listViewCustomer.Size = new System.Drawing.Size(741, 97);
+            this.listViewCustomer.TabIndex = 19;
+            this.listViewCustomer.UseCompatibleStateImageBehavior = false;
+            this.listViewCustomer.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Customer ID";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Phone Number";
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 548);
+            this.Controls.Add(this.listViewCustomer);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonListCustomers);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonAddToList);
+            this.Controls.Add(this.comboBoxChoice);
+            this.Controls.Add(this.maskedTextBoxPhoneNumber);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBoxCustomerId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCustomerId;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPhoneNumber;
+        private System.Windows.Forms.ComboBox comboBoxChoice;
+        private System.Windows.Forms.Button buttonAddToList;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonListCustomers;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ListView listViewCustomer;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
